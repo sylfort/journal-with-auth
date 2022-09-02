67684c7747db2b3cf0ff2c1dbddf38b9eddbb62a
synesdeija <thedeija@gmail.com>
2022-09-02 14:36:15 -0500

    feat: replaced Todo with entry

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,69 @@
 const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('span.not')
-const todoComplete = document.querySelectorAll('span.completed')
+const entryItem = document.querySelectorAll('span.not')
+const entryComplete = document.querySelectorAll('span.read')
 
-Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+Array.from(deleteBtn).forEach((el) => {
+    el.addEventListener('click', deleteEntry)
 })
 
-Array.from(todoItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
+Array.from(entryItem).forEach((el) => {
+    el.addEventListener('click', markRead)
 })
 
-Array.from(todoComplete).forEach((el)=>{
-    el.addEventListener('click', markIncomplete)
+Array.from(entryComplete).forEach((el) => {
+    el.addEventListener('click', markUnread)
 })
 
-async function deleteTodo(){
-    const todoId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('todos/deleteTodo', {
+async function deleteEntry() {
+    const entryId = this.parentNode.dataset.id
+    try {
+        const response = await fetch('Entries/deleteEntry', {
             method: 'delete',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'entryIdFromJSFile': entryId
             })
         })
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
-    const todoId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('todos/markComplete', {
+async function markRead() {
+    const entryId = this.parentNode.dataset.id
+    try {
+        const response = await fetch('Entries/markRead', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'entryIdFromJSFile': entryId
             })
         })
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markIncomplete(){
-    const todoId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('todos/markIncomplete', {
+async function markUnread() {
+    const entryId = this.parentNode.dataset.id
+    try {
+        const response = await fetch('Entries/markUnread', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                'entryIdFromJSFile': entryId
             })
         })
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
