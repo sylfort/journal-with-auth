08b84be8e2921b5cb19cfcb0073235a805c01053
Aloutfi <63522731+aloutfi@users.noreply.github.com>
2022-09-01 19:58:33 -0500

    fix: template env, docs, and .env file location

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config()
 
 // Passport config
 require('./config/passport')(passport)
