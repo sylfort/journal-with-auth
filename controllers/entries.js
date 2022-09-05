d3ba480fc64515b8b29cca4a98123b34a44ac9c3
rad-a <11688089+rad-a@users.noreply.github.com>
2022-09-04 20:48:45 -0500

    Added entry title and body

--- controllers/entries.js
+++ controllers/entries.js
@@ -6,14 +6,14 @@ module.exports = {
         try {
             const journalEntries = await Entry.find({ userId: req.user.id })
             const unreadEntries = await Entry.countDocuments({ userId: req.user.id, read: false })
-            res.render('Entries.ejs', { Entries: journalEntries, left: unreadEntries, user: req.user })
+            res.render('Entries.ejs', { Entries: journalEntries, left: unreadEntries, user: req.user }) // TODO: Remove journalEntries 
         } catch (err) {
             console.log(err)
         }
     },
     createEntry: async(req, res) => {
         try {
-            await Entry.create({ entry: req.body.entryItem, read: false, userId: req.user.id })
+            await Entry.create({ title: req.body.titleItem, entry: req.body.entryItem, read: false, userId: req.user.id })
             console.log('entry has been added!')
             res.redirect('/Entries')
         } catch (err) {
